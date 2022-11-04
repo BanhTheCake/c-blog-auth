b765b390746f66d7a9a45202167f3422bde1c160
Binhanh <binhanh080103@gmail.com>
2022-11-04 23:12:03 +0700

    Finish auth route

---
+++ server/src/models/users.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        trim: true,
+        required: [true, 'Username must be required !']
+    },
+    password: {
+        type: String,
+        trim: true,
+        required: [true, 'Password must be required !']
+    },
+    gmail: {
+        type: String,
+        trim: true,
+        required: [true, 'Gmail must be required !']
+    },
+}, {
+    timestamps: true,
+    collection: 'users'
+});
+
+const Users = mongoose.model('users', userSchema);
+module.exports = Users
+
+
